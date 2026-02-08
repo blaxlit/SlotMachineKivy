985230ffa33ac24cd598134db164462becc246e6
Thaweerat <thaweeratyordsathien@gmail.com>
2026-02-08 11:01:02 +0700

    Import GridLayout and Image

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.image import Image
 
 class SlotGame(BoxLayout):
     pass
